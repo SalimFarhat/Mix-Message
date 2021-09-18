faa02f684507b13b4114ea6c2269e9137bfcca22
Salim Farhat <salimfarhat@gmail.com>
2021-09-18 03:03:58 -0400

    third commit

--- MixMessage.js
+++ MixMessage.js
@@ -3,6 +3,7 @@ const firstMessage = 'Hello World I love you!'
 console.log(firstMessage)
 const firstNumber = Math.floor(Math.random() * 6)
 console.log(firstNumber)
-console.log(firstMessage)
-console.log('My god will this work!')
-// adding another comment
+const secondMessage = 'It worked so darn well'
+console.log(secondMessage)
+console.log('My god will this work! It certainly did')
+// adding another comment Just to see if it works!
