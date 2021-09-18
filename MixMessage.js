9fd226f82b8b986275514270a7d501877de1d2e0
Salim Farhat <salimfarhat@gmail.com>
2021-09-18 03:00:44 -0400

    second commit

--- MixMessage.js
+++ MixMessage.js
@@ -1,7 +1,8 @@
-// Adding a comment
-const firstMessage = 'Hello World'
+// Adding a comment. Or two or three
+const firstMessage = 'Hello World I love you!'
 console.log(firstMessage)
-const firstNumber = Math.floor(Math.random() * 5)
+const firstNumber = Math.floor(Math.random() * 6)
 console.log(firstNumber)
 console.log(firstMessage)
+console.log('My god will this work!')
 // adding another comment
