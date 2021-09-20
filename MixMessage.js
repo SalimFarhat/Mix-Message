5bcd1dca9b87e7b84675a648f655e043cd141223
Salim Farhat <salimfarhat@gmail.com>
2021-09-20 13:05:53 -0400

    final commit

--- MixMessage.js
+++ MixMessage.js
@@ -1,12 +1,12 @@
 // Adding a comment. Or two or three
 const messages = ['Happy wife, happy life', 'Don\'t drink and drive', 'We don\'t need no stinking badges!', 'Beam me up, Scotty', 'The sun is shining... but the ice is slippery', 'May the Force be with you', 'Play it again, Sam.', 'Love will find a way.', 'Kittens be cute', 'Dragons in heraldry', 'Uncles are cool'];
-let randomNumber = Math.floor(Math.random() * 11);
+let randomNumber = Math.floor(Math.random() * messages.length);
 let printedMessage = messages[randomNumber];
 document.getElementById("message").innerHTML = printedMessage
 
 function displayMessage(){
     const messages = ['Happy wife, happy life', 'Don\'t drink and drive', 'We don\'t need no stinking badges!', 'Beam me up, Scotty', 'The sun is shining... but the ice is slippery', 'May the Force be with you', 'Play it again, Sam.', 'Love will find a way.', 'Kittens be cute', 'Dragons in heraldry', 'Uncles are cool'];
-    let randomNumber = Math.floor(Math.random() * 11);
+    let randomNumber = Math.floor(Math.random() * messages.length);
     let printedMessage = messages[randomNumber];
     document.getElementById("message").innerHTML = printedMessage
 }
